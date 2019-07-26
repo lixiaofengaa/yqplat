8bd1519501fb70c9372e797a865b5ee6722ce841
ccc_glb <ccc_glb@qq.com>
2019-07-26 22:41:16 +0800

    odoo model property management

---
+++ property_app/models/__init__.py
@@ -0,0 +1,8 @@
+from . import property_base
+from . import property_charge
+from . import property_contract
+from . import property_material
+from . import property_maintain
+from . import property_decorate
+
+
