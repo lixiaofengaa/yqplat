5444d08bd2670a9559fdd4efca6736bf93d216e7
ccc_glb <ccc_glb@qq.com>
2019-08-07 08:53:03 +0800

    modified

--- odoomates/customO2O/shop_goods/__manifest__.py
+++ odoomates/customO2O/shop_goods/__manifest__.py
@@ -1,14 +1,13 @@
 {
-    'name': "商铺商品",
-    'summary': "后台管理：商铺商品管理",
-    'description': """Long description""",
-    'author': "万国车世界",
-    'license': "AGPL-3",
-    'website': "http://www.baidu.com",
-    'depends': ['base'],
+    'name': "店商", # 插件的标题
+    'summary': "店铺、商品、订单管理", # 副标题
+    'description': """Long description""", # 以RST格式书写的长描述，通常置于三引号中
+    'author': "ZYF", # 作者，多个作者时以逗号间隔
+    'license': "AGPL-3", # 证书，常用用AGPL-3，其它选项：LGPL-3或其它OSI证书
+    'website': "http://www.baidu.com", # 获取插件更多详情的网址
+    'depends': ['base'], # 所依赖的模块列表，如无，则至少会依赖base
     'application': True,
     'installable': True,
-    'auto_install': False,
     'data': [
         'security/shop_goods_security.xml',
         'security/ir.model.access.csv',
@@ -18,5 +17,5 @@
         'views/parent_menu_view.xml',
         'views/shop_view.xml'
         ],
-    'category': '万国',
+    # 'category': 'Library',
 }
--- odoomates/customO2O/shop_goods/models/shop_models.py
+++ odoomates/customO2O/shop_goods/models/shop_models.py
@@ -5,30 +5,30 @@ from odoo.exceptions import Warning
 class Shop(models.Model):
     _name = 'shop.view'
     _description = 'Shop'
-    name = fields.Char('商铺名称', required=True)
+    name = fields.Char('店铺名称', required=True)
     category = fields.Selection(
         [('cy', '餐饮'),
          ('yl', '娱乐'),
          ('qx', '汽修')],
         default='cy',
-        string='商铺类别'
+        string='店铺类别'
     )
     local = fields.Char('位置')
     dmxx = fields.Char('店面形象')
     business = fields.Char('主要业务')
-    score = fields.Integer(string='商铺评分')
-    grade = fields.Integer(string='商铺等级')
+    score = fields.Integer(string='店铺评分')
+    grade = fields.Integer(string='店铺等级')
     credibility = fields.Integer(string='信誉度')
     judge = fields.Selection(
         [('ff', '否'),
          ('yy', '是')],
         default='ff',
-        string='是否有实体商铺'
+        string='是否有实体店铺'
     )
     type = fields.Selection(
         [('zy', '自营'),
          ('st', '实体'),
          ('xn', '虚拟')],
         default='zy',
-        string='商铺类型'
+        string='店铺类型'
     )
